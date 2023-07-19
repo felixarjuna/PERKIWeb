4eac7efaed7ba3df59ee77cdd9d8f28a089e1d1a
Ricky Jonathan <jonathanricky12@gmail.com>
2023-07-19 15:34:16 +0200

    🍿add packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
+import React from "react";
 import './index.css';
 import Home from "./components/homepage";
 import AboutUs from "./components/about-us";
 import WhatWeDo from "./components/what-we-do";
-=======
-import React from "react";
-import AboutUs from "./components/aboutus";
-import Home from "./components/homepage";
 import { Mouse } from "./components/ui/mouse";
-import "./index.css";
->>>>>>> e3469abcd1f2f1b48dd06322bcea23f673d41db1
 
 type Position = {
   x: number;
@@ -31,12 +25,8 @@ function App() {
       <Mouse r={4} x={position.x} y={position.y} blur={false} />
       <Home />
       <AboutUs />
-<<<<<<< HEAD
       <WhatWeDo />
-    </>
-=======
     </div>
->>>>>>> e3469abcd1f2f1b48dd06322bcea23f673d41db1
   );
 }
 
