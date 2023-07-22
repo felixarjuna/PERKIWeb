ae9e43dcb80844a71e88a43dffb88165f7f5475b
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-07-22 11:31:02 +0200

    Update homepage.tsx

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -23,15 +23,15 @@ export default function Homepage() {
       </div>
 
       <div className="h-screen flex items-center justify-center flex-col border-t-[0.7px] border-black/10 text-green-default gap-7 ">
-        <h3 className="font-satoshi text-3xl">Welcome to our fellowship</h3>
-        <h1 className="font-lora text-[160px]">Perki Aachen</h1>
+        <h3 className="text-3xl uppercase">Welcome to our fellowship</h3>
+        <h1 className="text-[160px]">Perki Aachen</h1>
         <div className="flex gap-20">
-          <div className="flex items-center uppercase gap-2 text-2xl font-satoshi">
+          <div className="flex items-center uppercase gap-2 text-3xl">
             <p>since</p>
-            <div className="h-[1px] w-24 bg-green-default"></div>
-            <p className="font-satoshi">1990</p>
+            {/* <div className="h-[3px] w-24 bg-green-default"></div> */}
+            <p>1990</p>
           </div>
-          <div className="font-satoshi text-2xl uppercase">Non-denominational church</div>
+          <div className="text-3xl uppercase">Non-denominational church</div>
         </div>
       </div>
     </>
