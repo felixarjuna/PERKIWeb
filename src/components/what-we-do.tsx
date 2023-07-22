8fcc6fe653443e45f700457aa86af2a85cd35f8b
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-07-22 11:25:37 +0200

    🥪 Fix what we do

--- src/components/what-we-do.tsx
+++ src/components/what-we-do.tsx
@@ -1,62 +1,32 @@
 import Perki from "../images/S__3186691.jpg";
+import { events } from "../lib/events";
 
 export default function Home() {
   return (
-    <div>
-      <img src={Perki} alt="perki" />
-      <div className="bg-cream-default text-dark-green-default font-founders">
-        <div className="flex items-center gap-x-40">
-          <div className="-ml-16 -rotate-90 whitespace-nowrap h-fit">
-            <h1 className="text-5xl text-dark-green-default">What We Do?</h1>
-            <div className="h-[2px] bg-dark-green-default"></div>
-          </div>
+    <div className="bg-cream-default text-dark-green-default py-40">
+      <div className="flex items-center justify-center px-40 pb-40">
+        <img src={Perki} alt="perki" className="rounded-lg" />
+      </div>
 
-          <div className="grid grid-cols-2 gap-28 py-14">
-            <div className="max-w-xl">
-              <h1 className="text-4xl">Church Service / Big Group Bible Study</h1>
-              <div className="h-[2px] w-[312px] bg-dark-green-default -translate-y-1"></div>
-              <h2 className="text-3xl">Saturday, 15.30 - 17.00 </h2>
-              <p className="text-xl">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae architecto
-                delectus neque suscipit cum, consequatur eos cupiditate voluptates aliquam. Ipsum
-                cum officiis delectus recusandae molestiae obcaecati fugiat. Dolorem, atque
-                recusandae!
-              </p>
-            </div>
-            <div className="max-w-xl">
-              <h1 className="text-4xl">Small Group Bible Study</h1>
-              <div className="h-[2px] w-[312px] bg-dark-green-default -translate-y-1"></div>
-              <h2 className="text-3xl">Saturday, 13.00 - 15.00 </h2>
-              <p className="text-xl">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae architecto
-                delectus neque suscipit cum, consequatur eos cupiditate voluptates aliquam. Ipsum
-                cum officiis delectus recusandae molestiae obcaecati fugiat. Dolorem, atque
-                recusandae!
-              </p>
-            </div>
-            <div className="max-w-xl">
-              <h1 className="text-4xl">Prayer Fellowship (Aachen)</h1>
-              <div className="h-[2px] w-[312px] bg-dark-green-default -translate-y-1"></div>
-              <h2 className="text-3xl">Thursday, 19.00 - 21.00 </h2>
-              <p className="text-xl">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae architecto
-                delectus neque suscipit cum, consequatur eos cupiditate voluptates aliquam. Ipsum
-                cum officiis delectus recusandae molestiae obcaecati fugiat. Dolorem, atque
-                recusandae!
-              </p>
-            </div>
-            <div className="max-w-xl">
-              <h1 className="text-4xl">Prayer Fellowship (Jülich)</h1>
-              <div className="h-[2px] w-[312px] bg-dark-green-default -translate-y-1"></div>
-              <h2 className="text-3xl">Friday, 18.30 - 21.00 </h2>
-              <p className="text-xl">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae architecto
-                delectus neque suscipit cum, consequatur eos cupiditate voluptates aliquam. Ipsum
-                cum officiis delectus recusandae molestiae obcaecati fugiat. Dolorem, atque
-                recusandae!
-              </p>
-            </div>
-          </div>
+      <div className="flex items-center gap-x-32">
+        <div className="ml-16 -rotate-90 whitespace-nowrap h-fit">
+          <h1 className="text-5xl text-green-default">What We Do?</h1>
+          <div className="h-[2px] bg-green-default"></div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-28">
+          {events.map((event) => {
+            return (
+              <div className="max-w-xl">
+                <h1 className="text-3xl">{event.name}</h1>
+                {/* <div className="h-[2px] w-[312px] bg-dark-green-default -translate-y-1"></div> */}
+                <div className="mt-2 text-xl bg-green-default rounded-lg w-fit items-center flex text-cream-default p-2">
+                  <p>{event.time} </p>
+                </div>
+                <p className="text-xl mt-4">{event.description}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
---
+++ src/lib/events.ts
@@ -0,0 +1,27 @@
+export const events = [
+  {
+    name: "Church Service / Big Group Bible Study",
+    time: "Saturday, 15.30 - 17.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae architecto delectus neque suscipit cum, consequatur eos cupiditate voluptates aliquam. Ipsum cum officiis delectus recusandae molestiae obcaecati fugiat. Dolorem, atque recusandae!",
+  },
+  {
+    name: "Small Group Bible Study",
+    time: "Saturday, 15.30 - 17.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae architecto delectus neque suscipit cum, consequatur eos cupiditate voluptates aliquam. Ipsum cum officiis delectus recusandae molestiae obcaecati fugiat. Dolorem, atque recusandae!",
+  },
+  {
+    name: "Prayer Fellowship (Aachen)",
+    time: "Thursday, 18:30 - 21.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae architecto delectus neque suscipit cum, consequatur eos cupiditate voluptates aliquam. Ipsum cum officiis delectus recusandae molestiae obcaecati fugiat. Dolorem, atque recusandae!",
+  },
+
+  {
+    name: "Prayer Fellowship (Jülich)",
+    time: "Friday, 18.30 - 21.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae architecto delectus neque suscipit cum, consequatur eos cupiditate voluptates aliquam. Ipsum cum officiis delectus recusandae molestiae obcaecati fugiat. Dolorem, atque recusandae!",
+  },
+];
