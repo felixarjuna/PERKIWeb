b643b69f03dd817243157effc08927c4c435d002
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-07-22 11:08:22 +0200

    🥐 Update Homepage

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -1,18 +1,6 @@
-import React from "react";
-import { getVOTD } from "../api/api";
 import { CircleBackground } from "./ui/circle-background";
 
 export default function Homepage() {
-  const [verse, setVerse] = React.useState<string>("hello");
-
-  React.useEffect(() => {
-    const getVerse = async () => {
-      const verse = await getVOTD();
-      setVerse(verse);
-    };
-    getVerse();
-  }, []);
-
   return (
     <>
       <div className="overflow-hidden">
@@ -34,14 +22,6 @@ export default function Homepage() {
         />
       </div>
 
-      <div className="absolute top-10 h-[0.7px] bg-black/10 w-[90%] inset-x-0 mx-auto"></div>
-      <div className="absolute top-20 w-full">
-        <div className="relative flex overflow-hidden gap-1 text-3xl">
-          <div className="flex gap-4 animate-marquee whitespace-nowrap bg-black/10 w-full items-center justify-center uppercase">
-            <p className="">{verse}</p>
-          </div>
-        </div>
-      </div>
       <div className="h-screen flex items-center justify-center flex-col border-t-[0.7px] border-black/10 text-green-default gap-7 ">
         <h3 className="font-satoshi text-3xl">Welcome to our fellowship</h3>
         <h1 className="font-lora text-[160px]">Perki Aachen</h1>
