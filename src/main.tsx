17b626216afb3ad24d9e77b347925f4a8805903c
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-07-23 08:24:29 +0200

    🍠 Add React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Activity } from "./Activity/activity.tsx";
 import App from "./App.tsx";
 import "./index.css";
+import { Pastors } from "./pastors/pastors.tsx";
 
-const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/pastors", element: <Pastors /> },
+  { path: "/activity", element: <Activity /> },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
