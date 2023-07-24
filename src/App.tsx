b9331485918eada56008f7961669dcd18b968c73
Ricky Jonathan <jonathanricky12@gmail.com>
2023-07-24 20:21:36 +0200

    🥗map added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import AboutUs from "./components/about-us";
 import Home from "./components/homepage";
 import OurPastors from "./components/our-pastors";
 import WhatWeDo from "./components/what-we-do";
+import WhereAreWe from "./components/where-are-we";
 import "./index.css";
 import { Layout } from "./layout";
 
@@ -12,6 +13,7 @@ function App() {
       <AboutUs />
       <WhatWeDo />
       <OurPastors />
+      <WhereAreWe />
     </Layout>
   );
 }
