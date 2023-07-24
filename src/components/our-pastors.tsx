39024b849776725072b91b7585de9f12c05c71f3
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-07-24 22:21:06 +0200

    🧀 Update Where Are We

--- src/components/our-pastors.tsx
+++ src/components/our-pastors.tsx
@@ -15,7 +15,7 @@ export default function OurPastors() {
   return (
     <div className="bg-cream-default text-dark-green-default py-40">
       <div className="flex items-center">
-        <div className="ml-16 -rotate-90 whitespace-nowrap h-fit">
+        <div className="ml-7 -rotate-90 whitespace-nowrap h-fit">
           <h1 className="text-7xl text-green-default">Our Pastors</h1>
           {/* <div className="h-[2px] bg-green-default"></div> */}
         </div>
--- src/components/where-are-we.tsx
+++ src/components/where-are-we.tsx
@@ -1,11 +1,28 @@
+import { MapPin } from "lucide-react";
 import MapsicleMap from "../images/MapsicleMap.png";
 
 export default function Home() {
   return (
     <>
-      <div className=" bg-cream-default text-dark-green-default font-satoshi">
-        <h1 className="text-5xl font-bold text-center">Where Are We?</h1>
-        <img src={MapsicleMap} alt="MapsicleMap" className="object-cover h-screen w-screen"/>
+      <div className=" bg-cream-default text-dark-green-default">
+        <div className="relative">
+          <h1 className="text-7xl text-green-default text-center absolute inset-0 mx-auto top-10 ">
+            Where Are We
+          </h1>
+          <MapPin className="w-12 h-12 absolute inset-0 mx-auto my-auto left-16 bottom-12 text-green-default/80" />
+          <img src={MapsicleMap} alt="MapsicleMap" className="object-cover h-screen w-screen" />
+          <div className="absolute bg-green-default/80 h-full w-1/5 top-0 left-0 text-cream-default text-2xl flex flex-col justify-center gap-y-8 px-8">
+            <div>
+              <h1>FEG Aachen Germany</h1>
+              <p>Roermonderstr. 110</p>
+              <p>52072, Aachen</p>
+            </div>
+            <div>
+              <h1>Visit us on Saturday at 15:30</h1>
+              <p>See you there!</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
