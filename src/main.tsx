75a190bf8073a95660a6ba945fe086fb0006add7
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-08-04 19:57:30 +0200

    🐨 Add Prayers and Schedule Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { Activity } from "./Activity/activity.tsx";
 import App from "./App.tsx";
+import { Activity } from "./activity/activity.tsx";
 import "./index.css";
 import { Pastors } from "./pastors/pastors.tsx";
+import Prayers from "./prayers/prayers.tsx";
+import Schedule from "./schedule/schedule.tsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/pastors", element: <Pastors /> },
   { path: "/activity", element: <Activity /> },
+  { path: "/prayers", element: <Prayers /> },
+  { path: "/schedule", element: <Schedule /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
