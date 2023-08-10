608962e804bc8214f8a03ee3f921d49fb5c4d271
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-08-10 17:34:42 +0200

    🦘 Update Tabs Color

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -74,25 +74,27 @@ export default function Tabs({ tabs, className }: TabsProps) {
             key={tab.id}
             onClick={() => setActiveTab(tab.id)}
             className={cn(
-              "relative px-4 py-2 text-cream-default transition",
-              activeTab === tab.id && "hover:text-cream-default/60"
+              "relative px-4 py-2 text-cream-default isolate transition duration-500",
+              activeTab === tab.id && "text-green-default hover:text-green-default/80"
             )}
             style={{
               WebkitTapHighlightColor: "transparent",
             }}
           >
+            {tab.label}
+
             {activeTab === tab.id && (
               <motion.span
                 layoutId="bubble"
-                className="absolute inset-0 bg-cream-default z-10 mix-blend-difference"
+                className="absolute inset-0 -z-10 bg-cream-default"
                 style={{ borderRadius: 8 }}
                 transition={{ type: "spring", bounce: 0.2, duration: 0.8 }}
               />
             )}
-            <p>{tab.label}</p>
           </button>
         ))}
       </div>
+
       <div className="space-y-4">
         {contents
           .filter((x) => x.tabId === activeTab)
