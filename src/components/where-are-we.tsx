a84aeb3fc820251700f1f459e845705abc29b8b8
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-08-11 09:17:50 +0200

    Update where-are-we.tsx

--- src/components/where-are-we.tsx
+++ src/components/where-are-we.tsx
@@ -4,14 +4,11 @@ import MapsicleMap from "../assets/images/MapsicleMap.png";
 export default function Home() {
   return (
     <>
-      <div className="bg-cream-default text-dark-green-default">
+      <div className="bg-cream-default text-dark-green-default relative">
         <div className="relative">
-          <h1 className="text-9xl text-green-default text-center absolute inset-0 mx-auto top-20 font-reimbrandt xs:text-4xl xs:z-20">
-            Where Are We
-          </h1>
           <MapPin className="w-12 h-12 absolute inset-0 mx-auto my-auto left-16 bottom-12 text-green-default/80" />
           <img src={MapsicleMap} alt="MapsicleMap" className="object-cover h-screen w-screen" />
-          <div className="absolute bg-green-default/80 h-full w-[1/3] 2xl:w-1/4 bottom-0 left-0 text-cream-default text-2xl flex flex-col justify-center gap-y-8 px-16 2xl:px-20 z-10 xs:text-sm xs:w-full xs:h-1/3">
+          <div className="absolute bg-cream-default/60 h-full w-[1/3] 2xl:w-1/4 bottom-0 left-0 text-green-default text-2xl flex flex-col justify-center gap-y-8 px-16 2xl:px-20 z-0 xs:text-sm xs:w-full xs:h-1/3">
             <div>
               <h1>FEG Aachen Germany</h1>
               <p>Roermonderstr. 110</p>
@@ -22,6 +19,9 @@ export default function Home() {
               <p>See you there! 😉</p>
             </div>
           </div>
+          <h1 className="absolute text-9xl text-green-default text-center inset-0 mx-auto top-20 font-reimbrandt xs:text-4xl z-20 ">
+            Where Are We
+          </h1>
         </div>
       </div>
     </>
