f1fa94f52bac9736a1bae4213c157f7d2fda1a92
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-08-11 08:09:19 +0200

    🐳 Takeaway mobile version

--- src/takeaway/page.tsx
+++ src/takeaway/page.tsx
@@ -12,17 +12,17 @@ export default function Takeaway() {
     <section className="bg-dark-green-default text-cream-default pb-40 min-h-screen">
       <Navigation showNav={true} />
 
-      <div className="pt-20 px-24 flex flex-col items-center">
-        <div className="flex w-1/2 items-center justify-center gap-8 py-16 flex-col">
-          <h1 className="text-9xl font-reimbrandt">Takeaways</h1>
-          <div className="text-2xl flex flex-col gap-y-2">
+      <div className="pt-20 px-24 flex flex-col items-center xs:px-12 xs:pt-20">
+        <div className="flex w-1/2 items-center justify-center gap-8 py-16 flex-col xs:w-full xs:py-8">
+          <h1 className="text-9xl font-reimbrandt xs:text-4xl">Takeaways</h1>
+          <div className="text-2xl flex flex-col gap-y-2 xs:text-base">
             <p>“Your word is a lamp to my feet and a light to my path”</p>
             <p>– Psalm 119:105</p>
           </div>
         </div>
 
         <div className="mb-4">
-          <h3 className="text-2xl font-reimbrandt">
+          <h3 className="text-2xl font-reimbrandt xs:text-base">
             Let's share what you have learned, keep burning each other and grow together 🔥
           </h3>
         </div>
