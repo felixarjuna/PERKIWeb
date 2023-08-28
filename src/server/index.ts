dddfcb3d49e42b1ed4ac6d56b867a38fe31767c3
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-08-28 20:34:25 +0200

    🐻‍❄️ Make router scalable

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,25 +1,16 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
-import { publicProcedure, router } from "./trpc";
-
-import { todos } from "@/db/schema";
+import { router } from "./trpc";
 
 import Database from "better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
-import { z } from "zod";
+import { takeawayRouter } from "./api/routers/takeaway";
+import { todoRouter } from "./api/routers/todo";
 
 const sqlite = new Database("sqlite.db");
-const db = drizzle(sqlite);
+export const db = drizzle(sqlite);
 
 migrate(db, { migrationsFolder: "drizzle" });
 
-export const appRouter = router({
-  getTodos: publicProcedure.query(async () => {
-    return db.select().from(todos).all();
-  }),
-  addTodo: publicProcedure.input(z.string()).mutation(async (opts) => {
-    await db.insert(todos).values({ content: opts.input, done: 0 }).run();
-    return true;
-  }),
-});
+export const appRouter = router({ todos: todoRouter, takeaways: takeawayRouter });
 
 export type AppRouter = typeof appRouter;
