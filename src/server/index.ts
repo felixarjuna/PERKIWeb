cb70c27527b53e551816d98b2f46d1a4097624e0
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-08-28 20:48:01 +0200

    🐧 Remove migration

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,14 +2,11 @@ import { drizzle } from "drizzle-orm/better-sqlite3";
 import { router } from "./trpc";
 
 import Database from "better-sqlite3";
-import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import { takeawayRouter } from "./api/routers/takeaway";
 import { todoRouter } from "./api/routers/todo";
 
 const sqlite = new Database("sqlite.db");
 export const db = drizzle(sqlite);
-
-migrate(db, { migrationsFolder: "drizzle" });
 
 export const appRouter = router({ todos: todoRouter, takeaways: takeawayRouter });
 
