efc997dda99ea38893cdd8962197d1145a22fb5d
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-08-29 20:27:07 +0200

    🐘 Update tabs
    - Implement tRPC

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,119 +1,55 @@
 "use client";
 
-import { cn } from "@/lib/utils";
-import { motion } from "framer-motion";
+import { trpc } from "@/app/_trpc/client";
+import { dateTimeFormatter } from "@/lib/utils";
+import { isEmpty } from "lodash";
 import React from "react";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./select";
 
 interface TabsProps extends React.HTMLAttributes<HTMLDivElement> {
   tabs: { id: string; label: string }[];
 }
 
-const contents = [
-  {
-    tabId: "church-service",
-    title: "The heart of a disciple 1",
-    date: "Aug 24",
-    speaker: "Pdt. John Kusuma",
-    bibleVerse: "Genesis 1:26-28",
-    summary: "summary",
-    author: "Felix Arjuna",
-  },
-  {
-    tabId: "church-service",
-    title: "The heart of a disciple 2",
-    date: "Aug 24",
-    speaker: "Pdt. John Kusuma",
-    bibleVerse: "Genesis 1:26-28",
-    summary: "summary",
-    author: "Ricky Jonathan",
-  },
-  {
-    tabId: "bg-bible-study",
-    title: "hello world 1",
-    date: "Aug 24",
-    speaker: "Pdt. John Kusuma",
-    bibleVerse: "Genesis 1:26-28",
-    summary: "summary",
-    author: "Steffen Josua",
-  },
-  {
-    tabId: "bg-bible-study",
-    title: "hello world 2",
-    date: "Aug 24",
-    speaker: "Pdt. John Kusuma",
-    bibleVerse: "Genesis 1:26-28",
-    summary: "summary",
-    author: "Danny Kurniawan",
-  },
-  {
-    tabId: "sg-bible-study",
-    title: "Dosa",
-    date: "Aug 24",
-    speaker: "Group 1",
-    bibleVerse: "Genesis 1:26-28",
-    summary: "summary",
-    author: "Danny Kurniawan",
-  },
-  {
-    tabId: "sg-bible-study",
-    title: "Hamartia",
-    date: "Aug 24",
-    speaker: "Group 2",
-    bibleVerse: "Genesis 1:26-28 | Genesis 2:1-10",
-    summary: "summary",
-    author: "Danny Kurniawan",
-  },
-];
-
 export default function Tabs({ tabs, className }: TabsProps) {
-  const [activeTab, setActiveTab] = React.useState(tabs[0].id);
+  const { data } = trpc.takeaways.getTakeaways.useQuery();
+  const [takeawayId, setTakeawayId] = React.useState<string>("");
+
+  const takeaways = React.useMemo(() => {
+    return isEmpty(takeawayId)
+      ? data
+      : data?.filter((takeaway) => takeaway.takeawayId === takeawayId);
+  }, [data, takeawayId]);
 
   return (
-    <div className="space-y-8">
-      <div className={cn("flex space-x-1 xs:flex-col xs:text-sm", className)}>
-        {tabs.map((tab) => (
-          <button
-            key={tab.id}
-            onClick={() => setActiveTab(tab.id)}
-            className={cn(
-              "relative px-4 py-2 text-cream-default isolate transition duration-500",
-              activeTab === tab.id && "text-green-default hover:text-green-default/80"
-            )}
-            style={{
-              WebkitTapHighlightColor: "transparent",
-            }}
-          >
-            {tab.label}
-
-            {activeTab === tab.id && (
-              <motion.span
-                layoutId="bubble"
-                className="absolute inset-0 -z-10 bg-cream-default"
-                style={{ borderRadius: 8 }}
-                transition={{ type: "spring", bounce: 0.2, duration: 0.8 }}
-              />
-            )}
-          </button>
-        ))}
-      </div>
+    <div className="space-y-4">
+      <Select onValueChange={(value) => setTakeawayId(value)}>
+        <SelectTrigger>
+          <SelectValue placeholder="Fellowship type" />
+        </SelectTrigger>
+        <SelectContent>
+          {tabs.map((tab) => (
+            <SelectItem value={tab.id} key={tab.id}>
+              {tab.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
 
       <div className="space-y-4">
-        {contents
-          .filter((x) => x.tabId === activeTab)
-          .map((content, index) => {
-            return (
-              <TabContent
-                key={index}
-                tabId={content.tabId}
-                title={content.title}
-                date={content.date}
-                speaker={content.speaker}
-                bibleVerse={content.bibleVerse}
-                summary={content.summary}
-                author={content.author}
-              />
-            );
-          })}
+        {takeaways?.map((takeaway, index) => {
+          return (
+            <TabContent
+              key={index}
+              tabId={takeaway.takeawayId}
+              title={takeaway.title}
+              date={dateTimeFormatter(takeaway.date)}
+              speaker={takeaway.speaker}
+              bibleVerse={takeaway.bibleVerse}
+              summary={takeaway.summary}
+              contributors={takeaway.contributors}
+            />
+          );
+        })}
       </div>
     </div>
   );
@@ -126,23 +62,28 @@ interface TabContentProps {
   speaker: string;
   bibleVerse: string;
   summary: string;
-  author: string;
+  contributors: string;
 }
 
 function TabContent(props: TabContentProps) {
   return (
     <div className="p-6 bg-green-default/60 rounded-lg w-full hover:bg-green-default/80 shadow-lg transition duration-300 cursor-pointer xs:p-4">
-      <h1 className="text-2xl font-reimbrandt tracking-wide xs:text-lg">{props.title}</h1>
-      <div className="flex items-center gap-x-2 text-green-400/80 font-reimbrandt xs:text-xs xs:gap-1 xs:flex-wrap">
+      <h1 className="text-2xl font-reimbrandt tracking-wide xs:text-lg flex items-center justify-between">
+        {props.title}
+        <span className="bg-light-green-default text-green-default text-sm rounded-lg px-2 py-1 xs:text-xs xs:p-1">
+          {props.tabId}
+        </span>
+      </h1>
+      <div className="flex items-center gap-x-2 text-green-400/80 font-reimbrandt xs:text-xs xs:gap-1 xs:flex-wrap xs:mt-2">
         <p>{props.speaker}</p>
         <span>&middot;</span>
         <p>{props.bibleVerse}</p>
         <span>&middot;</span>
         <p>{props.date}</p>
         <span>&middot;</span>
-        <p>{props.author}</p>
       </div>
       <p className="mt-4 xs:text-sm">{props.summary}</p>
+      <p className="xs:text-xs text-green-400/80 mt-4">{props.contributors}</p>
     </div>
   );
 }
