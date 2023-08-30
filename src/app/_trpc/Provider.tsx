ca51a2d32df82f4c13cf0b2d0f8f0f752a420560
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-08-30 07:21:11 +0200

    🦄 Add ReactQueryDevTools

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { httpBatchLink } from "@trpc/client";
 import React from "react";
 import { trpc } from "./client";
@@ -13,7 +14,10 @@ export default function Provider({ children }: { children: React.ReactNode }) {
 
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </trpc.Provider>
   );
 }
