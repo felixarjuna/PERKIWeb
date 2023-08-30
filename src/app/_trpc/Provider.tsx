5fcb915f1315cf38949c43e907f82970840e1ee8
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-08-30 22:25:25 +0200

    🐻‍❄️ Update Base Url

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -6,10 +6,16 @@ import { httpBatchLink } from "@trpc/client";
 import React from "react";
 import { trpc } from "./client";
 
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+};
+
 export default function Provider({ children }: { children: React.ReactNode }) {
   const [queryClient] = React.useState(() => new QueryClient());
   const [trpcClient] = React.useState(() =>
-    trpc.createClient({ links: [httpBatchLink({ url: "http://localhost:3000/api/trpc" })] })
+    trpc.createClient({ links: [httpBatchLink({ url: `${getBaseUrl()}/api/trpc` })] })
   );
 
   return (
