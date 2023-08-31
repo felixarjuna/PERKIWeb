b50947acbde7d19b018ffd21f9c855a3c57780e2
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-08-31 20:59:40 +0200

    🦄 Update Todo Test Endpoint
    - Add mutation

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,15 +1,25 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { trpc } from "../_trpc/client";
 
 export default function TodoList() {
   const { data: todos } = trpc.todos.getTodos.useQuery();
+  const addTodo = trpc.todos.addTodo.useMutation();
+
   return (
     <div className="p-20 text-cream-default">
       <h2>Todo Test endpoint</h2>
       {todos?.map((todo, index) => {
         return <div key={index}>{JSON.stringify(todo)}</div>;
       })}
+      <Button
+        onClick={() => {
+          addTodo.mutate("hello world");
+        }}
+      >
+        Create new todo
+      </Button>
     </div>
   );
 }
