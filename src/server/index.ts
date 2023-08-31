882f0261df6601801dc76ede3f04e929b062be1c
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-08-31 06:21:56 +0200

    🐻 Update database connection

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,29 @@
-import { drizzle } from "drizzle-orm/better-sqlite3";
 import { router } from "./trpc";
 
-import Database from "better-sqlite3";
 import { prayerRouter } from "./api/routers/prayers";
 import { scheduleRouter } from "./api/routers/schedules";
 import { takeawayRouter } from "./api/routers/takeaway";
 import { todoRouter } from "./api/routers/todo";
 
-const sqlite = new Database("sqlite.db");
-export const db = drizzle(sqlite);
+// Development env
+// import { drizzle } from "drizzle-orm/better-sqlite3";
+// import Database from "better-sqlite3";
+// const sqlite = new Database("sqlite.db");
+// export const db = drizzle(sqlite);
+
+// Production env
+import { connect } from "@planetscale/database";
+import * as dotenv from "dotenv";
+import { drizzle } from "drizzle-orm/planetscale-serverless";
+dotenv.config();
+
+const connection = connect({
+  host: process.env["DATABASE_HOST"],
+  username: process.env["DATABASE_USERNAME"],
+  password: process.env["DATABASE_PASSWORD"],
+});
+
+export const db = drizzle(connection);
 
 export const appRouter = router({
   todos: todoRouter,
