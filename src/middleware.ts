9cdcd45c514902ef20ac638c94aa21c1786756c6
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-09-01 16:17:30 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
 
-export function middleware(request: NextRequest) {
-  const response = NextResponse.next();
-
-  if (request.nextUrl.pathname.startsWith("/api")) {
-    response.headers.append("Access-Control-Allow-Origin", "*");
-  }
-}
+export function middleware(request: NextRequest) {}
