aa5a5b9a22d545a4d50d56608c2f10c3dceaa070
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-09-06 18:29:35 +0200

    🐘 Add React Separator

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,9 +1,31 @@
-import { cn } from "~/lib/utils";
+"use client"
 
-export default function Separator({
-  className,
-}: React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div className={cn("h-[1px] w-40 bg-light-green-default", className)} />
-  );
-}
+import * as React from "react"
+import * as SeparatorPrimitive from "@radix-ui/react-separator"
+
+import { cn } from "~/lib/utils"
+
+const Separator = React.forwardRef<
+  React.ElementRef<typeof SeparatorPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
+>(
+  (
+    { className, orientation = "horizontal", decorative = true, ...props },
+    ref
+  ) => (
+    <SeparatorPrimitive.Root
+      ref={ref}
+      decorative={decorative}
+      orientation={orientation}
+      className={cn(
+        "shrink-0 bg-border",
+        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+        className
+      )}
+      {...props}
+    />
+  )
+)
+Separator.displayName = SeparatorPrimitive.Root.displayName
+
+export { Separator }
