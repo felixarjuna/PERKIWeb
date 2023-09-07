b6bd81d8a0cb8ae931f909c9240ea114449542b0
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-09-07 19:00:43 +0200

    🐧 Refactor Action Button

--- src/components/action-button.tsx
+++ src/components/action-button.tsx
@@ -23,36 +23,48 @@ export default function ActionButton({
 }: ActionButtonProps) {
   return (
     <div className={className}>
-      <div
-        className="flex h-6 w-6 items-center justify-center rounded-md bg-green-default/80 hover:bg-dark-green-default"
-        onClick={() => onEditClick()}
-      >
-        <PenBoxIcon className="h-4 w-4 xs:h-3 xs:w-3" />
-      </div>
-      <AlertDialog>
-        <AlertDialogTrigger>
-          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-red-300/30 hover:bg-red-300/50">
-            <TrashIcon className="h-4 w-4 xs:h-3 xs:w-3" />
-          </div>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This will permanently delete your entry and cannot be undone. 😥
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction
-              className="bg-red-300/50 hover:bg-red-300/30"
-              onClick={() => onDeleteClick()}
-            >
-              Continue
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+      <EditButton onEditClick={onEditClick} />
+      <DeleteButton onDeleteClick={onDeleteClick} />
     </div>
+  );
+}
+
+export function EditButton({ onEditClick }: { onEditClick: () => void }) {
+  return (
+    <div
+      className="flex h-6 w-6 items-center justify-center rounded-md bg-green-default/80 hover:bg-dark-green-default"
+      onClick={() => onEditClick()}
+    >
+      <PenBoxIcon className="h-4 w-4 xs:h-3 xs:w-3" />
+    </div>
+  );
+}
+
+export function DeleteButton({ onDeleteClick }: { onDeleteClick: () => void }) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <div className="flex h-6 w-6 items-center justify-center rounded-md bg-red-300/30 hover:bg-red-300/50">
+          <TrashIcon className="h-4 w-4 xs:h-3 xs:w-3" />
+        </div>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This will permanently delete your entry and cannot be undone. 😥
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            className="bg-red-300/50 hover:bg-red-300/30"
+            onClick={() => onDeleteClick()}
+          >
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
