2f65a41b9080f8d8d7deb78daca5e8e0e6736364
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-09-08 06:19:08 +0200

    🦧 Fix Tracking Layout and Hide in Mobile Screen

--- src/components/mouse-tracking-layout.tsx
+++ src/components/mouse-tracking-layout.tsx
@@ -8,20 +8,30 @@ type Position = {
   y: number;
 };
 
-export default function MouseTrackingLayout({ children }: { children: React.ReactNode }) {
+export default function MouseTrackingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const [position, setPosition] = React.useState<Position>({
     x: typeof window !== "undefined" ? window?.innerWidth / 2 : 0,
     y: typeof window !== "undefined" ? window?.innerHeight / 2 : 0,
   });
 
   const onMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
-    const { clientX, clientY } = event;
-    setPosition({ x: clientX, y: clientY });
+    const { pageX, pageY } = event;
+    setPosition({ x: pageX, y: pageY });
   };
 
   return (
     <div onMouseMove={onMouseMove} className="relative overflow-hidden">
-      <Mouse r={4} x={position.x} y={position.y} blur={false} />
+      <Mouse
+        r={4}
+        x={position.x}
+        y={position.y}
+        blur={false}
+        className="xs:hidden"
+      />
       {children}
     </div>
   );
