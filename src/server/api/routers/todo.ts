1daf9b0a9efa494c6f4c0ada87aea76c34e823d5
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-09-10 14:50:55 +0200

    🦬 Add protected api routes

--- src/server/api/routers/todo.ts
+++ src/server/api/routers/todo.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 import { todos } from "~/db/schema";
 import { db } from "~/server";
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const todoRouter = createTRPCRouter({
-  getTodos: publicProcedure.query(async () => {
+  getTodos: protectedProcedure.query(async () => {
     return await db.select().from(todos);
   }),
   addTodo: publicProcedure.input(z.string()).mutation(async (opts) => {
