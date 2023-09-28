bdf4ff7d0435a66b66336e92010c4b6fe52207be
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-09-28 15:59:05 +0200

    🐘 Add function to calculate username

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,26 +6,40 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const calculateXAxes = (step = 0.1, multiplier: number, start: number, end: number) => {
-  return Array.from({ length: Math.floor((end - start) / step) + 1 }, (_, index) => {
-    const scale = 2 / (3 - Math.cos(2 * index * step));
-    return scale * Math.cos(index * step) * multiplier;
-  });
+export const calculateXAxes = (
+  step = 0.1,
+  multiplier: number,
+  start: number,
+  end: number,
+) => {
+  return Array.from(
+    { length: Math.floor((end - start) / step) + 1 },
+    (_, index) => {
+      const scale = 2 / (3 - Math.cos(2 * index * step));
+      return scale * Math.cos(index * step) * multiplier;
+    },
+  );
 };
 
 export const calculateYAxes = (
   step = 0.1,
   multiplier: number,
   start: number = Math.PI,
-  end: number = -Math.PI
+  end: number = -Math.PI,
 ) => {
-  return Array.from({ length: Math.floor((end - start) / step) + 1 }, (_, index) => {
-    const scale = 2 / (3 - Math.cos(2 * index * step));
-    return ((scale * Math.sin(2 * step * index)) / 2) * multiplier;
-  });
+  return Array.from(
+    { length: Math.floor((end - start) / step) + 1 },
+    (_, index) => {
+      const scale = 2 / (3 - Math.cos(2 * index * step));
+      return ((scale * Math.sin(2 * step * index)) / 2) * multiplier;
+    },
+  );
 };
 
 export const dateTimeFormatter = (date: string) => {
   const datetime = DateTime.fromJSDate(new Date(date));
   return datetime.toFormat("LLL dd, yyyy");
 };
+
+export const getUsernameFromName = (name: string) =>
+  name?.replace(/\s/g, "").toLowerCase();
