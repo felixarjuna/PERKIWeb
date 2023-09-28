1fa9ade141465f2dd39f2bfa078b62d946c92e75
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-09-28 15:02:49 +0200

    🦒 Add SignIn Page

--- src/pages/auth/sign-in/index.tsx
+++ src/pages/auth/sign-in/index.tsx
@@ -12,9 +12,11 @@ export default function SignInPage({
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center text-cream-default">
-      <div className="w-full max-w-lg rounded-lg bg-green-default/60 p-8">
-        <h1 className="font-reimbrandt text-3xl">Sign in to PerkiWEB</h1>
-        <div className="mt-8 w-full text-cream-default">
+      <div className="w-full max-w-lg rounded-lg bg-green-default/60 p-8 xs:max-w-xs">
+        <h1 className="font-reimbrandt text-3xl xs:text-2xl">
+          Sign in to PerkiWEB
+        </h1>
+        <div className="w-full text-cream-default">
           <SignInProviders providers={providers} />
         </div>
       </div>
