f7179652faeb07eab59a36c50d40b0960373e09f
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-10-13 18:28:25 +0200

    🐘 Update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
 import * as dotenv from "dotenv";
-import { Config } from "drizzle-kit";
+import { type Config } from "drizzle-kit";
 dotenv.config();
 
 // Prod env
 export default {
-  schema: "./src/db/schema.ts",
-  out: "./drizzle",
+  schema: "./src/lib/db/schema",
+  out: "./src/lib/db/migrations",
   driver: "mysql2",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL ?? "",
