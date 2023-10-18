404aacdc39ed55d01754ad79f2fad3b9c7935cac
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-10-18 18:20:50 +0200

    🐺 Update users.ts
    - Enable user to update username and name

--- src/server/api/routers/users.ts
+++ src/server/api/routers/users.ts
@@ -19,8 +19,12 @@ export const userRouter = createTRPCRouter({
   getUsers: publicProcedure.query(async () => {
     return await db.select().from(users);
   }),
-  getUserById: publicProcedure.input(userIdSchema).query(({ input }) => {
-    return db.select().from(users).where(eq(users.id, input.id));
+  getUserById: publicProcedure.input(userIdSchema).query(async ({ input }) => {
+    const accounts = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, input.id));
+    return accounts.at(0) ?? null;
   }),
   createUser: publicProcedure
     .input(insertUserParams)
@@ -57,26 +61,32 @@ export const userRouter = createTRPCRouter({
         });
       }
     }),
+  /**
+   * User can only change name or username here.
+   * For password update, refer to "updatePassword" method
+   */
   updateUser: publicProcedure
     .input(updateUserParams)
     .mutation(async ({ input }) => {
-      const session = await getServerSession();
-      if (!session)
-        return new TRPCError({
-          code: "UNAUTHORIZED",
-          message: "User session not found",
+      // Return error if username or email already exists
+      const _users = db.select().from(users);
+
+      const isUsernameExists = await _users.where(
+        eq(users.email, input.username),
+      );
+      if (isUsernameExists.length > 0)
+        throw new TRPCError({
+          code: "CONFLICT",
+          message: "Username or email already exists",
         });
 
       const newUser = updateUserSchema.parse({
-        ...userIdSchema,
-        id: session.user.id,
+        ...input,
+        email: input.username,
       });
 
       try {
-        await db
-          .update(users)
-          .set(newUser)
-          .where(and(eq(users.id, input.id), eq(users.id, session.user.id)));
+        await db.update(users).set(newUser).where(eq(users.id, input.id));
         return { success: true };
       } catch (err) {
         const message = (err as Error).message ?? "Error, please try again";
