5db4c7046350d1c0f6b992c7b238df59939ead48
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-11-06 20:27:36 +0100

    🐅 Update add-takeaway-form.tsx
    - Simplify form to reference schedule selection
    - Rename summary to keypoints

--- src/pages/create-takeaway/add-takeaway-form.tsx
+++ src/pages/create-takeaway/add-takeaway-form.tsx
@@ -1,29 +1,22 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { format } from "date-fns";
-import { ArrowLeft, CalendarIcon } from "lucide-react";
+import { ArrowLeft } from "lucide-react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import React from "react";
 import { useForm } from "react-hook-form";
-import { type z } from "zod";
+import { z } from "zod";
 import { Button } from "~/components/ui/button";
-import { Calendar } from "~/components/ui/calendar";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "~/components/ui/form";
-import { Input } from "~/components/ui/input";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "~/components/ui/popover";
 import {
   Select,
   SelectContent,
@@ -33,10 +26,15 @@ import {
 } from "~/components/ui/select";
 import { Textarea } from "~/components/ui/textarea";
 import { useToast } from "~/components/ui/use-toast";
-import { speakers, takeawayIds } from "~/lib/data";
-import { cn, getUsernameFromName } from "~/lib/utils";
-import { addTakeawaySchema } from "~/server/api/schema/schema";
+import { getUsernameFromName } from "~/lib/utils";
+
 import { api } from "~/utils/api";
+
+const addTakeawayFormSchema = z.object({
+  scheduleTitle: z.string(),
+  keypoints: z.string(),
+  contributors: z.array(z.string()),
+});
 
 export default function AddTakeawayForm() {
   const { data: session } = useSession();
@@ -45,10 +43,12 @@ export default function AddTakeawayForm() {
     [session?.user.name],
   );
 
+  const { data: schedules } = api.schedules.getSchedules.useQuery();
+
   const { toast } = useToast();
   const router = useRouter();
 
-  const addTodo = api.takeaways.addTakeaway.useMutation({
+  const addTakeaway = api.takeaways.addTakeaway.useMutation({
     onSuccess: async () => {
       toast({
         title: "Your takeaway has been submitted! ✨",
@@ -59,8 +59,8 @@ export default function AddTakeawayForm() {
   });
 
   // 1. Define form.
-  const form = useForm<z.infer<typeof addTakeawaySchema>>({
-    resolver: zodResolver(addTakeawaySchema),
+  const form = useForm<z.infer<typeof addTakeawayFormSchema>>({
+    resolver: zodResolver(addTakeawayFormSchema),
     defaultValues: {
       // TODO: Automatically take contributors name from the username
       contributors: [username],
@@ -68,8 +68,12 @@ export default function AddTakeawayForm() {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof addTakeawaySchema>) {
-    addTodo.mutate({ ...values });
+  function onSubmit(values: z.infer<typeof addTakeawayFormSchema>) {
+    const scheduleId = schedules?.find(
+      (schedule) => schedule.title === values.scheduleTitle,
+    )?.id;
+    if (scheduleId === undefined) return;
+    addTakeaway.mutate({ ...values, scheduleId });
   }
 
   return (
@@ -83,149 +87,42 @@ export default function AddTakeawayForm() {
             Fellowship Information
           </h3>
           <section className="grid grid-cols-2 gap-4 xs:text-sm">
-            <div className="xs:col-span-2">
-              <FormField
-                control={form.control}
-                name="takeawayId"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-md">Fellowship Type</FormLabel>
-                    <FormControl>
-                      <Select
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select fellowship type" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          {takeawayIds.map((id, index) => (
-                            <SelectItem value={id} key={index}>
-                              {id}
-                            </SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-
-            <div className="xs:col-span-2">
-              <FormField
-                control={form.control}
-                name="title"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-md">Title</FormLabel>
-                    <FormControl>
-                      <Input {...field}></Input>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div className="xs:col-span-2">
-              <FormField
-                control={form.control}
-                name="date"
-                render={({ field }) => (
-                  <FormItem className="flex flex-col">
-                    <FormLabel className="text-md">Date</FormLabel>
-                    <FormControl>
-                      <Popover>
-                        <PopoverTrigger asChild>
-                          <FormControl>
-                            <Button
-                              variant={"default"}
-                              className={cn(
-                                "pl-3 text-left font-normal !text-cream-default",
-                                !field.value && "text-muted-foreground",
-                              )}
-                            >
-                              {field.value ? (
-                                format(field.value, "PPP")
-                              ) : (
-                                <span>Pick a date</span>
-                              )}
-                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                            </Button>
-                          </FormControl>
-                        </PopoverTrigger>
-                        <PopoverContent className="w-auto p-1" align="start">
-                          <Calendar
-                            mode="single"
-                            selected={field.value}
-                            onSelect={field.onChange}
-                            disabled={(date) =>
-                              date > new Date() || date < new Date("1900-01-01")
-                            }
-                            initialFocus
-                          />
-                        </PopoverContent>
-                      </Popover>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-
-            <div className="xs:col-span-2">
-              <FormField
-                control={form.control}
-                name="speaker"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-md">Speaker</FormLabel>
-                    <FormControl>
-                      <Select
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select a speaker for the service" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          {speakers.map((speaker, index) => (
-                            <SelectItem value={speaker} key={index}>
-                              {speaker}
-                            </SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div className="xs:col-span-2">
-              <FormField
-                control={form.control}
-                name="bibleVerse"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-md">Bible Verse</FormLabel>
-                    <FormControl>
-                      <Input {...field}></Input>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
             <div className="col-span-2">
               <FormField
                 control={form.control}
-                name="summary"
+                name="scheduleTitle"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-md">Schedule</FormLabel>
+                    <FormDescription className="text-xs">
+                      The schedule on which the keypoints should be based
+                    </FormDescription>
+                    <FormControl>
+                      <Select onValueChange={field.onChange}>
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          {schedules?.map((schedule, index) => (
+                            <SelectItem value={schedule.title} key={index}>
+                              {schedule.title}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="col-span-2">
+              <FormField
+                control={form.control}
+                name="keypoints"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel className="text-md">Key points</FormLabel>
