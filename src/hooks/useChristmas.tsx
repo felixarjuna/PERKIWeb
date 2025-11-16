c4e46ad278d03141a9e61679fd98945ade24a51b
Felix Arjuna <felixarjuna.de@yahoo.com>
2025-11-16 22:41:56 +0100

    feat: automatically send initial message

--- src/hooks/useChristmas.tsx
+++ src/hooks/useChristmas.tsx
@@ -5,13 +5,12 @@ import type { z } from "zod";
 import { toast } from "~/components/ui/use-toast";
 import type { addGuestSchema } from "~/pages/christmas";
 
+const APP_URL = "https://rsvp-perkiaachen.fly.dev";
 export const useChristmasGuestCount = () => {
   const { data: totalGuests, isLoading } = useQuery({
     queryKey: ["total-guests"],
     queryFn: async () => {
-      const response = await fetch(
-        "http://localhost:8000/api/guest/count?eventId=31"
-      );
+      const response = await fetch(`${APP_URL}/api/guest/count?eventId=31`);
       const data = await response.json();
       return data.data.count;
     },
@@ -23,22 +22,47 @@ export const useChristmasGuestCount = () => {
 export const useChristmasAddGuest = () => {
   const router = useRouter();
 
+  const sendInitialMessage = useMutation({
+    mutationKey: ["send-initial-message"],
+    mutationFn: async (phoneNumber: string) => {
+      await axios.post("http://localhost:8000/api/send-template", {
+        type: "initial",
+        phoneNumber,
+      });
+    },
+    onError: () => {
+      toast({
+        title: "⚠️ Failed to send initial message.",
+        description:
+          "There was an issue sending the initial message. Please contact support if you do not receive a message soon.",
+      });
+    },
+    onSuccess: () => {
+      toast({
+        title: "📱 Initial message sent.",
+        description:
+          "You should receive a message shortly with further details.",
+      });
+    },
+  });
+
   const { mutate, isLoading } = useMutation({
     mutationKey: ["add-christmas-guest"],
     mutationFn: async (request: z.infer<typeof addGuestSchema>) => {
-      await axios.post("http://localhost:8000/api/guest", {
+      await axios.post(`${APP_URL}/api/guest`, {
         method: "POST",
         data: request,
       });
     },
-    onSuccess: () => {
+    onSuccess: (_, variables) => {
+      /** send initial message */
+      sendInitialMessage.mutate(variables.phoneNumber);
+
       toast({
         title: "✅ Registration successful.",
         description: "Thank you for your registration!",
       });
       router.push("/christmas/thankyou");
-
-      /** send initial message */
     },
     onError: (err) => {
       if (axios.isAxiosError(err) && err.response) {
